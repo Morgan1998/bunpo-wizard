d460c2d7a39f588c07c005e64cc6eb01f61004bf
Morgan <morgancampbell0425@gmail.com>
2026-09-10 00:54:22 +0900

    chore: add createUser service

---
+++ server/src/services/user.service.ts
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt';
+import { db } from '../lib/db';
+import { type RegisterInput } from '../validators/auth.validator';
+
+export const createUser = async (input: RegisterInput) => {
+  const { username, email, password } = input;
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = await db.user.create({
+    data: {
+      username,
+      email,
+      passwordHash,
+    },
+    select: {
+      id: true,
+      username: true,
+      email: true,
+      createdAt: true,
+    },
+  });
+
+  return user;
+};
