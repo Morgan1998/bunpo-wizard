5ccef5372f516560a8c70e0318718c115598b3b4
Morgan <morgancampbell0425@gmail.com>
2026-09-12 02:23:10 +0900

    feat: strip out random stray import form battles controller

--- server/src/controllers/battles.controller.ts
+++ server/src/controllers/battles.controller.ts
@@ -1,6 +1,5 @@
 import { type Request, type Response, type NextFunction } from 'express';
 import * as battlesService from '../services/battles.service';
-import { threadName } from 'node:worker_threads';
 
 export const createBattle = async (
   req: Request,
@@ -9,7 +8,7 @@ export const createBattle = async (
 ): Promise<void> => {
   try {
     const battle = await battlesService.createBattle(
-      req.valid.body,
+      req.valid!.body,
       req.body.id,
     );
     res.status(201).json({ battle });
